d849d03589569778fe16aae62cac0552e54f581d
shower666 <ly6513@gmail.com>
2016-12-15 17:10:00 +0800

    主页面

--- FinalProject/app/src/main/java/com/example/finalproject/MainActivity.java
+++ FinalProject/app/src/main/java/com/example/finalproject/MainActivity.java
@@ -1,31 +1,170 @@
 package com.example.finalproject;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.GestureDetector;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
+    private TextView minutesView;
+    private TextView clock;
+    private Button startBtn;
+    private SeekBar seekBar;
+    private ImageView photo;
+    private int min;
+    private boolean flag;
+    private CountDownTimer countDownTimer;
+    private NumberFormat numberFormat = NumberFormat.getIntegerInstance();
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        numberFormat.setMinimumIntegerDigits(2);
+        sharedPreferences = this.getSharedPreferences("info", Context.MODE_PRIVATE);
+
+        findViews();
+        setupDrawerContent(navigationView);
+        setupListeners();
+    }
+
+    private void findViews() {
         drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
         navigationView = (NavigationView) findViewById(R.id.nvMenu);
+        minutesView = (TextView) findViewById(R.id.minutes);
+        clock = (TextView) findViewById(R.id.clock);
+        startBtn = (Button) findViewById(R.id.startBtn);
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        photo = (ImageView) findViewById(R.id.photo);
+    }
 
-        setupDrawerContent(navigationView);
+    private void setupListeners() {
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    min = progress / 5 * 5 + 20;
+                    clock.setText(min + ":00");
+                    if (min > 60) {
+                        photo.setImageResource(R.mipmap.happy);
+                    }
+                    else {
+                        photo.setImageResource(R.mipmap.smile);
+                    }
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!flag) {
+                    flag = true;
+                    seekBar.setVisibility(View.INVISIBLE);
+                    if (min > 60) {
+                        photo.setImageResource(R.mipmap.happy);
+                    }
+                    else {
+                        photo.setImageResource(R.mipmap.smile);
+                    }
+                    min *= 60;
+                    startBtn.setText("放弃");
+
+                    countDownTimer = new CountDownTimer(min * 60000, 1000) {
+                        @Override
+                        public void onTick(long millisUntilFinished) {
+                            if (min > 0) {
+                                min--;
+                                int s = min % 60;
+                                int m = min / 60;
+                                String temp = m + ":" + numberFormat.format(s);
+                                clock.setText(temp);
+                            }
+                        }
+
+                        @Override
+                        public void onFinish() {
+                            reset();
+                            flag = false;
+                        }
+                    };
+                    countDownTimer.start();
+                } else {
+                    failed();
+                }
+            }
+        });
+    }
+
+    private void failed() {
+        flag = false;
+        reset();
+        photo.setImageResource(R.mipmap.unhappy);
+        countDownTimer.cancel();
+    }
+
+    private void reset() {
+        seekBar.setVisibility(View.VISIBLE);
+        startBtn.setText("闭关");
+        min = seekBar.getProgress() / 5 * 5 + 20;
+        clock.setText(min + ":00");
+    }
+
+    @Override
+    protected void onStop() {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        if (flag) {
+            failed();
+            editor.putBoolean("flag", true);
+        } else {
+            editor.putBoolean("flag", false);
+        }
+        editor.putInt("min", min);
+        editor.commit();
+        super.onStop();
+    }
+
+    @Override
+    protected void onStart() {
+        min = sharedPreferences.getInt("min", 20);
+        flag = sharedPreferences.getBoolean("flag", false);
+        if (flag) {
+            photo.setImageResource(R.mipmap.unhappy);
+            seekBar.setProgress(min - 20);
+            flag = false;
+        }
+        super.onStart();
     }
 
     private void setupDrawerContent(NavigationView navigationView) {
@@ -57,25 +196,4 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
