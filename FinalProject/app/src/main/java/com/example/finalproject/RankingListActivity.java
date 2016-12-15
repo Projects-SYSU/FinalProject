acf499899f8085adb38fd3b04d5630d0552c18f3
gh835470669 <835470669@qq.com>
2016-12-15 21:41:01 +0800

    Ranking List Activity

--- FinalProject/app/src/main/java/com/example/finalproject/RankingListActivity.java
+++ FinalProject/app/src/main/java/com/example/finalproject/RankingListActivity.java
@@ -1,17 +1,142 @@
 package com.example.finalproject;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
+import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 public class RankingListActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
+    private ListView listView;
+    private List<Map<String, Object>> list_datas = new LinkedList();
+    private SimpleAdapter simpleAdapter;
+
+    private Handler handler = new Handler();
+
+    public class MyAdaptor extends BaseAdapter {
+        private Context context;
+        private List<Map<String, Object>> list;
+        private Handler hander;
+
+        public MyAdaptor(Context context, List<Map<String, Object>> list, Handler handler) {
+            this.context = context;
+            this.list = list;
+            this.hander = handler;
+        }
+
+        @Override
+        public int getCount() {
+            if (list == null) {
+                return  0;
+            }
+            return list.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            if (list == null) {
+                return null;
+            }
+            return list.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View view, ViewGroup parent) {
+            View convertView;
+            final ViewHolder viewHolder;
+
+            if (view == null) {
+                convertView = LayoutInflater.from(context).inflate(R.layout.rankinglistitem, null);
+                viewHolder = new ViewHolder();
+                viewHolder.name = (TextView)convertView.findViewById(R.id.ranking_list_name);
+                viewHolder.number = (TextView)convertView.findViewById(R.id.ranking_list_number);
+                viewHolder.point = (TextView)convertView.findViewById(R.id.ranking_list_point);
+                convertView.setTag(viewHolder);
+
+            } else {
+                convertView = view;
+                viewHolder = (ViewHolder)convertView.getTag();
+            }
+
+            viewHolder.name.setText((String)list.get(position).get("name"));
+            viewHolder.point.setText(Integer.toString((int)list.get(position).get("point")));
+            viewHolder.number.setText(Integer.toString((int)list.get(position).get("rank_num")));
+
+            if (position == 0) hander.post(new Runnable() {
+                @Override
+                public void run() {
+                    viewHolder.number.setBackgroundResource(R.drawable.gold);
+                    viewHolder.number.setText("");
+                }
+            });
+            if (position == 1) hander.post(new Runnable() {
+                @Override
+                public void run() {
+                    viewHolder.number.setBackgroundResource(R.drawable.silver);
+                    viewHolder.number.setText("");
+                }
+            });
+            if (position == 2) hander.post(new Runnable() {
+                @Override
+                public void run() {
+                    viewHolder.number.setBackgroundResource(R.drawable.copper);
+                    viewHolder.number.setText("");
+                }
+            });
+
+            return convertView;
+        }
+
+        public class ViewHolder {
+            public TextView name;
+            public TextView number;
+            public TextView point;
+        }
+    }
+
+    public static final class ComparatorValues implements Comparator<Map<String, Object>> {
+
+        @Override
+        public int compare(Map<String, Object> object1, Map<String, Object> object2) {
+            if((int)object1.get("point") > (int)object2.get("point")) {
+                return 1;
+            } else if ((int)object1.get("point") > (int)object2.get("point")) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +145,38 @@ public class RankingListActivity extends AppCompatActivity {
 
         drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
         navigationView = (NavigationView) findViewById(R.id.nvMenu);
+        listView = (ListView)findViewById(R.id.rankingList);
 
         setupDrawerContent(navigationView);
+
+        init_list_data();
+        sort_list_data();
+        set_adaptot();
+    }
+
+    private void set_adaptot() {
+        listView.setAdapter(new MyAdaptor(this, list_datas, handler));
+    }
+
+    private void sort_list_data() {
+        Collections.sort(list_datas, new ComparatorValues());
+
+        //add ranking number
+        for (int i = 0; i < list_datas.size(); ++i) {
+            list_datas.get(i).put("rank_num", i + 1);
+        }
+    }
+
+    private void init_list_data() {
+        int data_num = 10;
+        for (int i = 0; i < data_num; ++i) {
+            Map<String, Object> t = new HashMap<>();
+            t.put("name", String.format("N%d", i));
+            t.put("nei", 100 + i);
+            t.put("qing", 20000 + i);
+            t.put("point", 5000 + i);
+            list_datas.add(t);
+        }
     }
 
     private void setupDrawerContent(NavigationView navigationView) {
