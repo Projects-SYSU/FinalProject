8300ace57c258c757e4e457e3d2a936a0ea43afd
gh835470669 <835470669@qq.com>
2016-12-16 18:53:34 +0800

    Ranking List Activity（除了网络访问） *布局的重新设计 *ListView 前三名显示金银铜王冠 *ListView的下拉刷新功能（我的亮点）

--- FinalProject/app/src/main/java/com/example/finalproject/RankingListActivity.java
+++ FinalProject/app/src/main/java/com/example/finalproject/RankingListActivity.java
@@ -1,12 +1,14 @@
 package com.example.finalproject;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
@@ -16,6 +18,7 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
@@ -29,51 +32,34 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-public class RankingListActivity extends AppCompatActivity {
+public class RankingListActivity extends AppCompatActivity implements RefreshListView.IRefreshListener{
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
-    private ListView listView;
     private List<Map<String, Object>> list_datas = new LinkedList();
-    private SimpleAdapter simpleAdapter;
+    private Map<String, Object> ME = new HashMap<>();
+    private TextView myrank;
+    private TextView mypoint;
+    private MyAdaptor myAdaptor;
+    private RefreshListView refreshListView;
 
-    private Handler handler = new Handler();
-
-    private static class mRunnable implements Runnable {
-        MyAdaptor.ViewHolder viewHolder;
-        int rank;
-
-        public mRunnable(MyAdaptor.ViewHolder viewHolder, int rank) {
-            this.viewHolder = viewHolder;
-            this.rank = rank;
-        }
-
-        @Override
-        public void run() {
-            if (rank == 1) {
-                viewHolder.number.setBackgroundResource(R.drawable.gold);
-                viewHolder.number.setText("");
-            } else if (rank == 2) {
-                viewHolder.number.setBackgroundResource(R.drawable.silver);
-                viewHolder.number.setText("");
-            } else if (rank == 3) {
-                viewHolder.number.setBackgroundResource(R.drawable.copper);
-                viewHolder.number.setText("");
-            } else {
-                //nothing
+    @Override
+    public void onRefresh() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                update_data();
             }
-
-        }
+        }, 2000);
     }
 
-    public static class MyAdaptor extends BaseAdapter {
+    public class MyAdaptor extends BaseAdapter {
         private Context context;
         private List<Map<String, Object>> list;
-        private Handler hander;
 
-        public MyAdaptor(Context context, List<Map<String, Object>> list, Handler handler) {
+        public MyAdaptor(Context context, List<Map<String, Object>> list) {
             this.context = context;
             this.list = list;
-            this.hander = handler;
         }
 
         @Override
@@ -107,7 +93,7 @@ public class RankingListActivity extends AppCompatActivity {
                 convertView = LayoutInflater.from(context).inflate(R.layout.rankinglistitem, parent, false);
                 viewHolder = new ViewHolder();
                 viewHolder.name = (TextView)convertView.findViewById(R.id.ranking_list_name);
-                viewHolder.number = (TextView)convertView.findViewById(R.id.ranking_list_number);
+                viewHolder.rank = (TextView)convertView.findViewById(R.id.ranking_list_rank);
                 viewHolder.point = (TextView)convertView.findViewById(R.id.ranking_list_point);
                 convertView.setTag(viewHolder);
 
@@ -118,26 +104,30 @@ public class RankingListActivity extends AppCompatActivity {
 
             viewHolder.name.setText((String)list.get(position).get("name"));
             viewHolder.point.setText(Integer.toString((int)list.get(position).get("point")));
-            viewHolder.number.setText(Integer.toString((int)list.get(position).get("rank_num")));
-            viewHolder.number.setBackgroundResource(0);  //remove the background
+            viewHolder.rank.setText(Integer.toString((int)list.get(position).get("rank_num")));
+            viewHolder.rank.setBackgroundResource(0);  //remove the background
 
             //set NO.1 NO.2 and NO.3 picture
             if (position == 0) {
-                hander.post(new mRunnable(viewHolder, 1));
+                //hander.post(new mRunnable(viewHolder, 1));
+                viewHolder.rank.setBackgroundResource(R.drawable.gold);
+                viewHolder.rank.setText("");
             }
             if (position == 1) {
-                hander.post(new mRunnable(viewHolder, 2));
+                viewHolder.rank.setBackgroundResource(R.drawable.silver);
+                viewHolder.rank.setText("");
             }
             if (position == 2) {
-                hander.post(new mRunnable(viewHolder, 3));
+                viewHolder.rank.setBackgroundResource(R.drawable.copper);
+                viewHolder.rank.setText("");
             }
 
             return convertView;
         }
 
-        public static class ViewHolder {
+        public class ViewHolder {
             public TextView name;
-            public TextView number;
+            public TextView rank;
             public TextView point;
         }
     }
@@ -162,19 +152,69 @@ public class RankingListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ranking_list);
 
-        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
-        navigationView = (NavigationView) findViewById(R.id.nvMenu);
-        listView = (ListView)findViewById(R.id.rankingList);
+        findView();
 
         setupDrawerContent(navigationView);
 
+        set_mydata();
+
         init_list_data();
         sort_list_data();
-        set_adaptot();
+        myAdaptor = new MyAdaptor(this, list_datas);
+        refreshListView.setAdapter(myAdaptor);
+        refreshListView.setInterface(RankingListActivity.this);
+        refreshListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //set dialoglayout
+                RelativeLayout rl_dia = (RelativeLayout)LayoutInflater.from(RankingListActivity.this)
+                        .inflate(R.layout.rankinglist_dialog, null);
+                TextView nei = (TextView)rl_dia.findViewById(R.id.rl_dialog_nei);
+                TextView qing = (TextView)rl_dia.findViewById(R.id.rl_dialog_qing);
+                nei.setText(list_datas.get(position).get("nei").toString() + "分");
+                qing.setText(list_datas.get(position).get("qing").toString() + "步");
+
+                new AlertDialog.Builder(RankingListActivity.this)
+                        .setTitle(list_datas.get(position).get("name").toString())
+                        .setPositiveButton("确认", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                //do nothing
+                            }
+                        })
+                        .setView(rl_dia)
+                        .show();
+            }
+        });
     }
 
-    private void set_adaptot() {
-        listView.setAdapter(new MyAdaptor(this, list_datas, handler));
+    private void findView() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        navigationView = (NavigationView) findViewById(R.id.nvMenu);
+        //listView = (ListView)findViewById(R.id.rankingList);
+        refreshListView = (RefreshListView)findViewById(R.id.rankingList);
+        myrank = (TextView)findViewById(R.id.ranking_list_myrank);
+        mypoint = (TextView)findViewById(R.id.ranking_list_mypoint);
+    }
+
+    private void set_mydata() {
+        ME.put("name", "N5");
+        ME.put("point", 5005);
+        ME.put("qing", 20005);
+        ME.put("nei", 105);
+
+        mypoint.setText(String.valueOf(5005));
+    }
+
+    private void update_data() {
+        get_data(); //from server? now nothing
+        sort_list_data();
+        myAdaptor.notifyDataSetChanged();
+        refreshListView.refreshComplete();
+    }
+
+    private void get_data() {
+        list_datas.get(0).put("point", (int)list_datas.get(0).get("point") - 10);
     }
 
     private void sort_list_data() {
@@ -183,6 +223,9 @@ public class RankingListActivity extends AppCompatActivity {
         //add ranking number
         for (int i = 0; i < list_datas.size(); ++i) {
             list_datas.get(i).put("rank_num", i + 1);
+            if (list_datas.get(i).get("name").toString().equals(ME.get("name"))) {
+                myrank.setText(String.valueOf(i + 1));
+            }
         }
     }
 
